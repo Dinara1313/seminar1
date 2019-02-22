618b91ff3fa35e3f4f043e8a27c3e54e33965c51
Diana1313 <di13021985@gmail.com>
2019-02-22 18:46:02 +0300

    8 - 9

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# seminar1
+Kurmantaeva Diana
+Dinara1313
+http://github.com/Dinara1313/seminar1.git
---
+++ main.py
@@ -0,0 +1,6 @@
+from myclass import Mobile_phone
+
+phone = Mobile_phone()
+phone.read_parameters()
+phone.change_parameters("white", "Samsung", 64, 1)
+phone.read_parameters()
